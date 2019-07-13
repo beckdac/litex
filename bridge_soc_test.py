142844d7230102cfdaf6be40946ae83cd9946ba8
Dave Beck <beck.dac@live.com>
2019-07-13 02:52:07 -0700

    register read out and knight rider lights working

--- bridge_soc_test.py
+++ bridge_soc_test.py
@@ -3,18 +3,48 @@ import time
 
 import litex
 
-wb = litex.RemoteClient()
-wb.open()
 
-wb.read(0x82000000, 1)
+def display_wb_info(wb):
+    for entry in wb.items:
+        if entry[0] == 'csr_base':
+            pass
+        elif entry[0] == 'csr_register':
+            res = wb.read(int(entry[2], 16), int(entry[3]))
+            print("csr %30s  %10s  %8d  %s  =  %s" % 
+                    (entry[1], entry[2], int(entry[3]), entry[4], res))
+        elif entry[0] == 'constant':
+            pass
+        elif entry[0] == 'memory_region':
+            print("mem %30s  %10s  %8d  %s" % (entry[1], entry[2], int(entry[3]), entry[4]))
 
-i = 0
-dir = -1
-while (1):
-    bits = (1 << i) ^ 0xFF
-    wb.write(0x82000000, bits)
-    time.sleep(.150)
-    if i == 7 or i == 0:
-        dir *= -1
-    i = i + dir
-    #print(f"{i} {dir}")
+
+def main():
+    print("be sure you started the litex_server for bridging, e.g.")
+    print("litex_server --uart --uart-port /dev/ttyUSB0  --uart-baudrate 115200")
+    print("---")
+
+    wb = litex.RemoteClient()
+    wb.open()
+    display_wb_info(wb)
+
+    i = 0
+    dir = -1
+    top = 100
+    counter = 0
+    while (1):
+        bits = (1 << i) ^ 0xFF
+        wb.write(0x82000000, bits)
+        time.sleep(.150)
+        if i == 7 or i == 0:
+            dir *= -1
+        i = i + dir
+
+        if counter == top:
+            counter = 0
+            display_wb_info(wb)
+        else:
+            counter = counter + 1
+
+
+if __name__ == "__main__":
+    main()
